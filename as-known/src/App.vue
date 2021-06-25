70e6c9b8faa3d0f4b68cfd4891d2a5ca4e9db4cf
v_naawang <v_naawang@tencent.com>
2021-06-25 14:34:17 +0800

    创建asKnown项目

---
+++ as-known/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <global-header></global-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import GlobalHeader from './components/GlobalHeader.vue'
+
+export default defineComponent({
+  name:'App',
+  components: {
+    GlobalHeader
+  }
+})
+</script>
+
+
+<style lang="less">
+*{
+  margin: 0;
+  padding: 0;
+}
+li{
+  list-style: none;
+}
+</style>
